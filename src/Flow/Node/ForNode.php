ed6c1bf11ede827931077de77cd0920b3d0e413d
Nofriandi Ramenta <nofriandi@ramenta.com>
2015-02-27 20:48:58 +0700

    Fix broken for-else construct

--- src/Flow/Node/ForNode.php
+++ src/Flow/Node/ForNode.php
@@ -34,7 +34,7 @@ class ForNode extends Node
 
         $else = false;
         if (!is_null($this->else)) {
-            $compiler->raw('if (!Flow\Helper\is_empty(', $indent);
+            $compiler->raw('if (!Flow\Helper::is_empty(', $indent);
             $this->seq->compile($compiler);
             $compiler->raw(")) {\n");
             $else = true;
