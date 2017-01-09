04af9e7e754a9878f886e721d4ed63f1f0b767ec
Nofriandi Ramenta <nofriandi@ramenta.com>
2017-01-09 14:06:56 +0700

    Rename member variable to better reflect its usage and abstraction

--- src/Flow/Adapter/FileAdapter.php
+++ src/Flow/Adapter/FileAdapter.php
@@ -6,31 +6,31 @@ use Flow\Adapter;
 
 final class FileAdapter implements Adapter
 {
-    private $source;
+    private $directory;
 
-    public function __construct($source)
+    public function __construct($directory)
     {
-        if (!($this->source = realpath($source)) || !is_dir($this->source)) {
+        if (!($this->directory = realpath($directory)) || !is_dir($this->directory)) {
             throw new \RuntimeException(sprintf(
-                'source directory %s not found',
-                $source
+                'directory %s not found',
+                $directory
             ));
         }
     }
 
     public function isReadable(string $path) : bool
     {
-        return is_readable($this->source. '/' . $path);
+        return is_readable($this->directory . '/' . $path);
     }
 
     public function lastModified(string $path) : int
     {
-        return filemtime($this->source. '/' . $path);
+        return filemtime($this->directory . '/' . $path);
     }
 
     public function getContents(string $path) : string
     {
-        return file_get_contents($this->source . '/' . $path);
+        return file_get_contents($this->directory . '/' . $path);
     }
 }
 
